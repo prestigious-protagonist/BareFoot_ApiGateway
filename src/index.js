67f47b368a61ab889af4363e588b2d065d9e4137
prestigious-protagonist <jaskaranyt123@gmail.com>
2025-05-26 09:56:33 +0530

    Adding Apigateway for barefoot

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit") 
+const axios = require('axios')
+const app = express();
+const checkJwt = require("./middleware/auth")
+const {PRODUCT_SERVICE_BASE_URL, CART_SERVICE_BASE_URL, ORDER_SERVICE_BASE_URL, PORT} = require("./config/server-config")
+app.use(morgan('combined'))
+// app.use(rateLimiter({
+//   windowMs: 2*60*1000,
+//   max: 5
+// }))
+
+app.use(cors({
+  origin: "*"
+}))
+app.get('/', (req, res)=>{
+  res.send("Apigateway...")
+})
+
+
+app.use("/productService",checkJwt,(req, res, next) => {
+  
+  if (req.auth?.sub) {
+    console.log(req.headers["authorization"])
+    req.headers["x-user-id"] = req.auth.sub;
+  }
+  next();
+}, createProxyMiddleware({
+  target: PRODUCT_SERVICE_BASE_URL,
+  changeOrigin: true,
+  
+}));
+
+
+app.use("/cartService",checkJwt,(req, res, next) => {
+  
+  if (req.auth?.sub) {
+    req.headers["x-user-id"] = req.auth.sub;
+  }
+  next();
+}, createProxyMiddleware({
+  target: CART_SERVICE_BASE_URL,
+  changeOrigin: true,
+  
+}));
+
+app.use("/orderService",checkJwt,(req, res, next) => {
+  
+  if (req.auth?.sub) {
+    req.headers["x-user-id"] = req.auth.sub;
+  }
+  next();
+}, createProxyMiddleware({
+  target: ORDER_SERVICE_BASE_URL,
+  changeOrigin: true,
+  
+}));
+
+app.listen(PORT, () => {
+  console.log(`API Gateway listening on port ${PORT}...`);
+});
